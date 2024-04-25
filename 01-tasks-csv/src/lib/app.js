445c08f51290d29df7a3148b3d5ac8bf0efbaeed
vhcsilva <vhcsilvaa@gmail.com>
2024-04-25 18:35:31 -0300

    feat: add json middleware

--- 01-tasks-csv/src/lib/app.js
+++ 01-tasks-csv/src/lib/app.js
@@ -1,8 +1,10 @@
 import http from 'node:http'
+import { Logger } from './logger.js'
 
 export class App {
   #server = null
   #routes = []
+  #middlewares = []
 
   constructor() {
     this.#server = http.createServer(async (req, res) => {
@@ -10,8 +12,15 @@ export class App {
       const route = this.#routes.find(route => route.method === method && route.path === url)
       if (!route)
         return res.writeHead(404).end()
+      for (const middleware of this.#middlewares) {
+        await middleware(req, res)
+      }
       return route.handler(req, res)
     })
+  }
+
+  use(fn) {
+    this.#middlewares.push(fn)
   }
 
   get(path, handler) {
@@ -58,6 +67,6 @@ export class App {
     if (!port || port < 1)
       throw new Error("Invalid port")
     this.#server.listen(3333)
-    console.log(`[SERVER] Server listening on port ${port}`)
+    Logger.log(`Server listening on port ${port}`)
   }
 }
--- 01-tasks-csv/src/server.js
+++ 01-tasks-csv/src/server.js
@@ -1,5 +1,8 @@
 import { App } from './lib/app.js'
+import { json } from './middlewares/json.js'
 
 const app = new App()
+
+app.use(json)
 
 app.listen(3333)
