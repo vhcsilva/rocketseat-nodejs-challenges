95fae2d8ea1a750a0bfd6da25e1337ad135ae164
vhcsilva <vhcsilvaa@gmail.com>
2024-04-29 20:59:13 -0300

    refactor: create TaskController class and use JsonDatabase

--- 01-tasks-csv/src/controllers/task-controller.js
+++ 01-tasks-csv/src/controllers/task-controller.js
@@ -1,91 +1,83 @@
 import { randomUUID } from 'node:crypto'
 import { HttpBadRequestError, HttpNotFoundError } from '../protocols/http-errors.js'
-import { lowerCaseIncludes } from '../utils/string.js'
 
-const tasks = []
+export class TaskController {
+  #table = 'tasks'
+  #database
 
-async function listTasks(req, res) {
-  const { title, description } = req.query
-  
-  const list = !!title || !!description ? 
-    tasks.filter(task => lowerCaseIncludes(task.title, title) || lowerCaseIncludes(task.description, description)) : tasks
-
-  return res.end(JSON.stringify(list))
-}
-
-async function addTask(req, res) {
-  const { title, description } = req.body
-
-  if (!title)
-    throw new HttpBadRequestError('Missing title')
-
-  if (!description)
-    throw new HttpBadRequestError('Missing description')
-
-  const newTask = {
-    id: randomUUID(),
-    title,
-    description,
-    created_at: new Date(),
-    updated_at: new Date(),
-    completed_at: null,
+  constructor(database) {
+    this.#database = database
   }
 
-  tasks.push(newTask)
+  async list(req, res) {
+    const { title, description } = req.query
 
-  return res.writeHead(201).end(JSON.stringify(newTask))
-}
+    const search = !!title || !!description ? { title, description } : null
+    const tasks = this.#database.select(this.#table, search)
+  
+    return res.end(JSON.stringify(tasks))
+  }
 
-async function updateTask(req, res) {
-  const { id } = req.params
-  const { title, description } = req.body
-
-  if (!title && !description)
-    throw new HttpBadRequestError('Missing parameters')
-
-  const task = tasks.find(task => task.id === id)
-
-  if (!task)
-    throw new HttpNotFoundError('Task not found')
-
-  if (title)
-    task.title = title
-  else if (description)
-    task.description = description
-
-  return res.writeHead(200).end()
-}
-
-async function deleteTask(req, res) {
-  const { id } = req.params
-
-  const taskIndex = tasks.findIndex(task => task.id === id)
-
-  if (taskIndex <= -1)
-    throw new HttpNotFoundError('Task not found')
-
-  tasks.splice(taskIndex, 1)
-
-  return res.end()
-}
-
-async function markAsCompleted(req, res) {
-  const { id } = req.params
-
-  const task = tasks.find(task => task.id === id)
-
-  if (!task)
-    throw new HttpNotFoundError('Task not found')
-
-  task.completed_at = new Date()
-
-  return res.writeHead(200).end()
-}
-
-export const TaskController = {
-  listTasks,
-  addTask,
-  updateTask,
-  deleteTask,
-  markAsCompleted,
+  async add(req, res) {
+    const { title, description } = req.body
+  
+    if (!title)
+      throw new HttpBadRequestError('Missing title')
+  
+    if (!description)
+      throw new HttpBadRequestError('Missing description')
+  
+    const newTask = {
+      id: randomUUID(),
+      title,
+      description,
+      created_at: new Date(),
+      updated_at: new Date(),
+      completed_at: null,
+    }
+  
+    this.#database.insert(this.#table, newTask)
+  
+    return res.writeHead(201).end(JSON.stringify(newTask))
+  }
+  
+  async update(req, res) {
+    const { id } = req.params
+    const { title, description } = req.body
+  
+    if (!title && !description)
+      throw new HttpBadRequestError('Missing parameters')
+  
+    if (!this.#database.select(this.#table, { id }))
+      throw new HttpNotFoundError('Task not found')
+  
+    if (title)
+      this.#database.update(this.#table, id, { title })
+    else if (description)
+      this.#database.update(this.#table, id, { description })
+  
+    return res.writeHead(200).end()
+  }
+  
+  async delete(req, res) {
+    const { id } = req.params
+  
+    if (!this.#database.select(this.#table, { id }))
+      throw new HttpNotFoundError('Task not found')
+  
+    this.#database.delete(this.#table, id)
+  
+    return res.end()
+  }
+  
+  async markAsCompleted(req, res) {
+    const { id } = req.params
+  
+    if (!this.#database.select(this.#table, { id }))
+      throw new HttpNotFoundError('Task not found')
+  
+    this.#database.update(this.#table, id, { completed_at: new Date() })
+  
+    return res.writeHead(200).end()
+  }
 }
